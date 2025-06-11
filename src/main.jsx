466da02f2bddb9cef069e193f25d34a74ff195d2
user.name "avinash <avinash>
2025-06-12 05:17:49 +0530

    C-6th

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import ContextProvider from './components/Context.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/chatbot">
   <ContextProvider>
     <App />
   </ContextProvider>
