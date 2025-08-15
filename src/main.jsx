42ce2ff6abdd27fe940d3dd59711187f6b5362f0
avinash <avinashraj0918@gmail.com>
2025-08-16 03:51:53 +0530

    updated routers again [nested brow.Router]

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,10 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
+  <BrowserRouter>
   <ContextProvider>
     <App />
   </ContextProvider>
+  </BrowserRouter>
   </HashRouter>
 )
