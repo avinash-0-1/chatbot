a4949275bb17811cc4a0fa38099781f9b5242014
avinash <avinashraj0918@gmail.com>
2025-08-16 02:43:56 +0530

    updated pub.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  // base:"/ai-baba"
+  plugins: [react(), tailwindcss()],
+  base: "./" // ensures assets load correctly in static hosting
 })
