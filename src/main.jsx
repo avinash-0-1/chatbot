386cc288af45599ac934d518f954591c2a77d66a
avinash <avinashraj0918@gmail.com>
2025-08-16 03:42:32 +0530

    updated brow.Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import ContextProvider from './components/Context.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
   <ContextProvider>
     <App />
   </ContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 )
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "./" // ensures assets load correctly in static hosting
+  base: "/" // ensures assets load correctly in static hosting
 })
